cbcc4228774ad4f91116a991fbe92a7fed7b5582
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-26 00:03:51 -0700

    Added BucketSubsystem stub

---
+++ PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/BucketSubsystem.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveDriveConstants;
+
+import com.revrobotics.spark.SparkLowLevel;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+
+
+public class BucketSubsystem extends SubsystemBase {
+
+  public BucketSubsystem() {
+  }
+}
