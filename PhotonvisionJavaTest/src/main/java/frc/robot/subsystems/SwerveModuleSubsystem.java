00d985100fc381516b64b25f8f57da0b7511f2bb
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-25 22:48:26 -0700

    Finished Constructor for SwerveModuleSubsystem

--- PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveModuleSubsystem.java
+++ PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveModuleSubsystem.java
@@ -4,12 +4,48 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.NetworkTableInstance.NetworkMode;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveDriveConstants;
+
+import com.revrobotics.spark.SparkLowLevel;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+
 
 public class SwerveModuleSubsystem extends SubsystemBase {
+  
+  TalonFX drive;
+  SparkMax turn;
+  RelativeEncoder turnEncoder;
+  ProfiledPIDController turningPIDController;
+  double maxOut;
+
   /** Creates a new SwerveModuleSubsystem. */
-  public SwerveModuleSubsystem() {}
+  public SwerveModuleSubsystem(int driveID, int steerID, double P, double I, double D) {
+    this.drive = new TalonFX(driveID, "talonfx");
+    this.drive.setNeutralMode(NeutralModeValue.Brake);
+
+    this.turn = new SparkMax(steerID, SparkLowLevel.MotorType.kBrushless);
+    this.turnEncoder = this.turn.getEncoder(); // Zero wheels before power on
+
+    this.turningPIDController = new ProfiledPIDController(
+      P, I, D,
+      new TrapezoidProfile.Constraints(
+        SwerveDriveConstants.MODULE_MAX_ANGULAR_VELOCITY,
+        SwerveDriveConstants.MODULE_MAX_ANGULAR_ACCELERATION
+      )
+
+    );
+    this.turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+
+    this.maxOut = 0;
+  }
 
   /**
    * Example command factory method.
