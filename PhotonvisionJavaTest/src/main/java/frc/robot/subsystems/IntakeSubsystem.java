b0d0737447eaea2579e645cc48027393080c3c22
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-26 00:24:22 -0700

    Intake broken due to deprication issues, will go back to later

---
+++ PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorIDConstants;
+import frc.robot.Constants.StateConstants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  // private TalonSRX motorLeft;
+  // private TalonSRX motorRight;
+
+  // private PWMTalonSRX motors;
+
+  // public IntakeSubsystem() {
+  //   super();
+
+  //   this.motorLeft = new TalonSRX(MotorIDConstants.INTAKE_LEFT_ID);
+
+  //   this.motorRight = new TalonSRX(MotorIDConstants.INTAKE_RIGHT_ID);
+  //   this.motorRight.setInverted(true);
+
+  //   this.motors = new PWMTalonSRX(this.motorLeft, this.motorRight);
+  // }
+
+  // public void setSpeed(double speed) {
+  //   this.motors.set(speed);
+  // }
+}
