aa54987514606f4d75e0bd6995b9df068c2b7f54
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-25 23:44:43 -0700

    Finished Constructor and setMaxOutput method for SwerveDriveSubsystem

--- PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -4,28 +4,82 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.networktables.NetworkTableInstance.NetworkMode;
-import edu.wpi.first.wpilibj2.command.Command;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import com.kauailabs.navx.frc;
+
+import frc.robot.Constants.MotorIDConstants;
+import frc.robot.Constants.PIDConstants;
+import frc.robot.Constants.SpeedConstants;
 import frc.robot.Constants.SwerveDriveConstants;
-import pabeles.concurrency.IntOperatorTask.Max;
-
-import com.revrobotics.spark.SparkLowLevel;
-
-import static edu.wpi.first.units.Units.Rotation;
-
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkMax;
 
 
 public class SwerveDriveSubsystem extends SubsystemBase {
+
+  private Translation2d frontLeftLocation;
+  private Translation2d frontRightLocation;
+  private Translation2d backLeftLocation;
+  private Translation2d backRightLocation;
+
+  private SwerveModuleSubsystem frontLeftModule;
+  private SwerveModuleSubsystem frontRightModule;
+  private SwerveModuleSubsystem backLeftModule;
+  private SwerveModuleSubsystem backRightModule;
+
+  private boolean turnLock;
+  private AHRS gyro;
+
+  private double maxOut;
   
   public SwerveDriveSubsystem() {
+    final double P = PIDConstants.P;
+    final double I = PIDConstants.I;
+    final double D = PIDConstants.D;
+    final double ctc2 = SwerveDriveConstants.SWERVE_MOD_CENTER_TO_CENTER / 2.0;
+
+    this.frontLeftLocation = new Translation2d(ctc2, ctc2);
+    this.frontRightLocation = new Translation2d(ctc2, -ctc2);
+    this.backLeftLocation = new Translation2d(-ctc2, ctc2);
+    this.backRightLocation = new Translation2d(-ctc2, -ctc2);
+
+    this.frontLeftModule = new SwerveModuleSubsystem(
+      MotorIDConstants.FRONT_LEFT_DRIVE_ID, 
+      MotorIDConstants.FRONT_LEFT_TURN_ID, 
+      P, I, D
+    );
+
+    this.frontRightModule = new SwerveModuleSubsystem(
+      MotorIDConstants.FRONT_RIGHT_DRIVE_ID, 
+      MotorIDConstants.FRONT_RIGHT_TURN_ID, 
+      P, I, D
+    );
+
+    this.backLeftModule = new SwerveModuleSubsystem(
+      MotorIDConstants.BACK_LEFT_DRIVE_ID, 
+      MotorIDConstants.BACK_LEFT_TURN_ID, 
+      P, I, D
+    );
+
+    this.backRightModule = new SwerveModuleSubsystem(
+      MotorIDConstants.BACK_RIGHT_DRIVE_ID, 
+      MotorIDConstants.BACK_RIGHT_TURN_ID, 
+      P, I, D
+    );
+
+    this.turnLock = false;
+    this.gyro = new AHRS(NavXComType.kMXP_SPI);
+
+    this.setMaxOutput(SpeedConstants.SWERVE_MAX_OUTPUT);
+  }
+
+  public void setMaxOutput(double maxOutput) {
+    this.maxOut = maxOutput;
+    this.frontLeftModule.setMaxOut(maxOutput);
+    this.frontRightModule.setMaxOut(maxOutput);
+    this.backLeftModule.setMaxOut(maxOutput);
+    this.backRightModule.setMaxOut(maxOutput);
   }
 }
