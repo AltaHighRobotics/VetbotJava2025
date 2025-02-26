d8081f866a4d731d5d6f76a0b4aa50fb81b1eb34
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-25 23:04:25 -0700

    Finished SwerveModuleSubsystem, not being used but tests pass

--- PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveModuleSubsystem.java
+++ PhotonvisionJavaTest/src/main/java/frc/robot/subsystems/SwerveModuleSubsystem.java
@@ -6,11 +6,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.NetworkTableInstance.NetworkMode;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SwerveDriveConstants;
+import pabeles.concurrency.IntOperatorTask.Max;
 
 import com.revrobotics.spark.SparkLowLevel;
 
@@ -61,37 +63,25 @@ public class SwerveModuleSubsystem extends SubsystemBase {
     this.maxOut = value;
   }
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
+  void setDesiredState(final SwerveModuleState desiredState) {
+    final double tau = Math.PI * 2;
+    final double gearRatio = SwerveDriveConstants.SWERVE_TURN_GEAR_RATIO;
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
+    final Rotation2d encoderRotation = this.getEncoder();
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    SwerveModuleState state = desiredState;
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    state.angle = desiredState.angle.times(-1);
+    state.optimize(encoderRotation);
+    state.speedMetersPerSecond *= state.angle.minus(encoderRotation).getCos();
+
+    final double driveOuput = state.speedMetersPerSecond;
+    
+    final double turnOutput = this.turningPIDController.calculate(
+      this.turnEncoder.getPosition() * tau * gearRatio, state.angle.getRadians()
+    );
+
+    this.drive.set(Math.max(-this.maxOut, Math.min(driveOuput, this.maxOut)));
+    this.turn.setVoltage(turnOutput);
   }
 }
