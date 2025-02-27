3ab666db3043f17db4eec48c6228df4d4f5ae4fc
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-27 14:25:55 -0700

    Cleaned up robotContainer

--- Robot/src/main/java/frc/robot/RobotContainer.java
+++ Robot/src/main/java/frc/robot/RobotContainer.java
@@ -27,10 +27,6 @@ import frc.robot.subsystems.SwerveDriveSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
   private final Joystick driverController;
 
   private SwerveDriveSubsystem drive;
@@ -49,17 +45,6 @@ public class RobotContainer {
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be
-   * created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-   * an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link
-   * CommandXboxController
-   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or
-   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
