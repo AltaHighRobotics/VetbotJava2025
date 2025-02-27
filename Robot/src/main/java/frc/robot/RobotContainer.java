8d5c8ac4b2eec5ee9c52cda3d11ea60e75840deb
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-27 14:26:47 -0700

    Removed unnessessary imports

--- Robot/src/main/java/frc/robot/RobotContainer.java
+++ Robot/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.InputConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.SwerveDriveCommand;
