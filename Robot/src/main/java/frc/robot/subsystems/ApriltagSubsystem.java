0b5a005e13a73149bb8125191449caf32ff62a9d
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-27 12:12:16 -0700

    Cleaned up ApriltagSubsystem and added getFirstTarget method

--- Robot/src/main/java/frc/robot/subsystems/ApriltagSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ApriltagSubsystem.java
@@ -51,6 +51,11 @@ public class ApriltagSubsystem extends SubsystemBase {
     }
   }
 
+  public PhotonTrackedTarget getFirstTarget() {
+    if (targets.size() == 0) { return null; }
+    return targets.get(0);
+  }
+
   public double getTargetYaw(int id) {
     List<PhotonTrackedTarget> newTargets = this.targets;
     if (newTargets != null) {
@@ -62,39 +67,5 @@ public class ApriltagSubsystem extends SubsystemBase {
     }
 
     return 0;
-  }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
