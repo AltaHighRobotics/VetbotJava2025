741095a818dd204b27a52f521e87c2b6967e1dbe
Nathan Young <yyou9481138@gmail.com>
2025-02-28 13:27:00 -0700

    removed uneccessary code

--- Robot/src/main/java/frc/robot/commands/MoveForward.java
+++ Robot/src/main/java/frc/robot/commands/MoveForward.java
@@ -23,44 +23,4 @@ public class MoveForward extends Command {
         drive.drive(driveYSpeed, driveXSpeed, driveRotation, driveSpeed);
     }
 
-
-    // @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    // private final SwerveDriveSubsystem driveSubsystem;
-    // private Joystick driverController;
-
-    // /**
-    //  * Creates a new ExampleCommand.
-    //  *
-    //  * @param subsystem The subsystem used by this command.
-    //  */
-    // public SwerveDriveCommand(SwerveDriveSubsystem driveSubsystem, Joystick driverController) {
-    //   this.driveSubsystem = driveSubsystem;
-    //   this.driverController = driverController;
-    //   addRequirements(driveSubsystem);
-    // }
-
-    // // Called when the command is initially scheduled.
-    // @Override
-    // public void initialize() {}
-
-    // // Called every time the scheduler runs while the command is scheduled.
-    // @Override
-    // public void execute() {
-    //   final double forwardSpeed = this.driverController.getX();
-    //   final double strafeSpeed = this.driverController.getY();
-    //   final double rotationSpeed = this.driverController.getZ();
-    //   final double speedScaling = (-(this.driverController.getRawAxis(3)) + 1.0) / 2.0;
-    
-    //   this.driveSubsystem.drive(forwardSpeed, strafeSpeed, rotationSpeed, speedScaling);
-    // }
-
-    // // Called once the command ends or is interrupted.
-    // @Override
-    // public void end(boolean interrupted) {}
-
-    // // Returns true when the command should end.
-    // @Override
-    // public boolean isFinished() {
-    //   return false;
-    // }
 }
