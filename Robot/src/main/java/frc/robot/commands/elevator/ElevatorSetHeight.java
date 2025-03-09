979267ba9b5ed3785645c6adc03470f666bb952d
Ethanscharlie <ethanscharlie@protonmail.com>
2025-03-09 14:56:44 -0600

    Added ElevatorSetHeight command

---
+++ Robot/src/main/java/frc/robot/commands/elevator/ElevatorSetHeight.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorSetHeight extends Command {
+    ElevatorSubsystem subsystem;    
+    double targetHeightPercentage;
+
+    public ElevatorSetHeight(ElevatorSubsystem subsystem, double targetHeightPercentage) {
+        this.subsystem = subsystem;
+
+        if (targetHeightPercentage < 0 || targetHeightPercentage > 1) {
+            throw new Error("Target Height for ElevatorSetHeight must be between 0 and 1");
+        }
+
+        this.targetHeight = targetHeightPercentage;
+    }
+
+    @Override
+    public execute() {
+        this.subsystem.setHeight(this.targetHeightPercentage);
+    }
+}
