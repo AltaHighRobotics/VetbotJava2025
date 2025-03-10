127d899aad8824720db3c88fef15645ffc8879fc
Ethanscharlie <ethanscharlie@protonmail.com>
2025-03-10 11:21:30 -0600

    Elevator uses seperate controllered pid

--- Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -15,7 +15,6 @@ public class ElevatorSubsystem extends SubsystemBase{
     private SparkFlex motorController1;
     private SparkFlex motorController2;
 
-    private RelativeEncoder encoder;
     private PIDController pidController;
 
     private double targetHeightPercentage = 0.0;
@@ -24,9 +23,6 @@ public class ElevatorSubsystem extends SubsystemBase{
         super();
         this.motorController1 = new SparkFlex(ElevatorConstants.TURN_1_ID, MotorType.kBrushless);
         this.motorController2 = new SparkFlex(ElevatorConstants.TURN_2_ID, MotorType.kBrushless);
-
-
-        this.encoder = motorController1.getEncoder();
 
         final double P = ElevatorConstants.P;
         final double I = ElevatorConstants.I;
@@ -52,34 +48,37 @@ public class ElevatorSubsystem extends SubsystemBase{
 
     public void moveToTargetHeight() { // Meant to be called each tick
         final double targetPositionRevolutions = targetHeightPercentage * ElevatorConstants.TOP_MAG;
-        final double currentPositionRevolutions = this.getHeight();
+        final double currentPositionRevolutions1 = this.getHeight(this.motorController1.getEncoder());
+        final double currentPositionRevolutions2 = this.getHeight(this.motorController2.getEncoder());
 
         final double newP = SmartDashboard.getEntry("Elevator P").getDouble(0);
         final double newI = SmartDashboard.getEntry("Elevator I").getDouble(0);
         final double newD = SmartDashboard.getEntry("Elevator D").getDouble(0);
         this.pidController.setPID(newP, newI, newD);
 
-        double motorOutput = this.pidController.calculate(currentPositionRevolutions, targetPositionRevolutions);
-        System.out.printf("Elevator Motor raw: %.6f\n", motorOutput);
+        double motorOutput1 = this.pidController.calculate(currentPositionRevolutions1, targetPositionRevolutions);
+        double motorOutput2 = this.pidController.calculate(currentPositionRevolutions2, -targetPositionRevolutions);
+        System.out.printf("Elevator Motor raw: %.6f\n", motorOutput1);
 
         // Clamp limits the motor speed, should probably use the max output speed but oh well this works too
-        motorOutput = MathUtil.clamp(motorOutput, -ElevatorConstants.MOTOR_SPEED, ElevatorConstants.MOTOR_SPEED);
+        motorOutput1 = MathUtil.clamp(motorOutput1, -ElevatorConstants.MOTOR_SPEED, ElevatorConstants.MOTOR_SPEED);
+        motorOutput2 = MathUtil.clamp(motorOutput2, -ElevatorConstants.MOTOR_SPEED, ElevatorConstants.MOTOR_SPEED);
 
         // Make it move half as fast when going down (Gravity makes it go down quicker)
         // if (motorOutput < 0) {
         //     motorOutput *= 0.5;
         // }
 
-        motorController1.setVoltage(motorOutput);
-        motorController2.setVoltage(-motorOutput);
+        motorController1.setVoltage(motorOutput1);
+        motorController2.setVoltage(motorOutput2);
     }
 
     /**
      * Gets the current height of the elevator
      * @return Height as revolutions
      */
-    public double getHeight() {
-        final double realMotorAngle = this.encoder.getPosition();
+    public double getHeight(RelativeEncoder encoder) {
+        final double realMotorAngle = encoder.getPosition();
         return realMotorAngle;
     }
 
@@ -87,8 +86,8 @@ public class ElevatorSubsystem extends SubsystemBase{
      * Gets the current height of the elevator
      * @return Height as percentage (0.0 to 1.0)
      */
-    public double getHeightAsPercentage() {
-        return getHeight() / ElevatorConstants.TOP_MAG;
+    public double getHeightAsPercentage(RelativeEncoder encoder) {
+        return getHeight(encoder) / ElevatorConstants.TOP_MAG;
     }
 
     public void goUp() {
@@ -100,7 +99,8 @@ public class ElevatorSubsystem extends SubsystemBase{
     }
 
     public void stop() {
-        targetHeightPercentage = getHeightAsPercentage();
+        targetHeightPercentage = getHeightAsPercentage(this.motorController1.getEncoder());
+        targetHeightPercentage = getHeightAsPercentage(this.motorController2.getEncoder());
         motorController1.set(0);
         motorController2.set(0);
     }
