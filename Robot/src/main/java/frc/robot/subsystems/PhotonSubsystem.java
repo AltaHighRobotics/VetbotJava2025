915fed5e417e1e6977a181479b510e051e37f805
Duncan <143680478+Tiny0pus@users.noreply.github.com>
2025-03-13 15:14:44 -0600

    PhotonSubsystem has a getTarget method

--- Robot/src/main/java/frc/robot/subsystems/PhotonSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/PhotonSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class PhotonSubsystem extends SubsystemBase {
     private PhotonCamera camera;
@@ -9,4 +11,10 @@ public class PhotonSubsystem extends SubsystemBase {
     public PhotonSubsystem() {
         this.camera = new PhotonCamera("photonvision");
     }    
+
+    public PhotonTrackedTarget getTarget() {
+        PhotonPipelineResult result = camera.getLatestResult();
+        if (!result.hasTargets()) { return null; }
+        return result.getBestTarget();
+    }
 }
